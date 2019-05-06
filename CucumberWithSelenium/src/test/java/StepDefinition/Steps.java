dcfcbe6f165c1d2f2ffcd2088e90065dd444bd19
Vijay_Chaudhary <Vijay_Chaudhary@W2650341GCWAS.SYMC.SYMANTEC.COM>
2019-05-06 13:58:12 +0530

    commit#1

---
+++ CucumberWithSelenium/src/test/java/StepDefinition/Steps.java
@@ -0,0 +1,48 @@
+package StepDefinition;		
+
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+
+import cucumber.api.java.en.Given;		
+import cucumber.api.java.en.Then;		
+import cucumber.api.java.en.When;		
+
+public class Steps {				
+
+    WebDriver driver;			
+    		
+    @Given("^Open the Firefox and launch the application$")					
+    public void open_the_Firefox_and_launch_the_application() throws Throwable							
+    {		
+       //System.setProperty("webdriver.gecko.driver", "E://Selenium//Selenium_Jars//geckodriver.exe");					
+		DesiredCapabilities caps = DesiredCapabilities.chrome();
+		caps.setPlatform(Platform.LINUX);
+		//driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), caps);
+		driver = new RemoteWebDriver(new URL("http://redzonetest2/wd/hub"), caps);
+
+       //driver= new FirefoxDriver();					
+       driver.manage().window().maximize();			
+       driver.get("http://demo.guru99.com/v4");					
+    }		
+
+    @When("^Enter the Username and Password$")					
+    public void enter_the_Username_and_Password() throws Throwable 							
+    {		
+       driver.findElement(By.name("uid")).sendKeys("username12");							
+       driver.findElement(By.name("password")).sendKeys("password12");							
+    }		
+
+    @Then("^Reset the credential$")					
+    public void Reset_the_credential() throws Throwable 							
+    {		
+       driver.findElement(By.name("btnReset")).click();		
+       driver.quit();
+    }		
+}
